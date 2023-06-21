a2f9cee7c75de155e22d62bccc104ac720837648
khacodee <114900252+khacodee@users.noreply.github.com>
2023-06-21 11:58:07 +0700

    Login form

--- RedisChat/RedisChat.Client/HomeForm.Designer.cs
+++ RedisChat/RedisChat.Client/HomeForm.Designer.cs
@@ -28,226 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            PanelRegister = new Panel();
-            txtRegisterLastName = new TextBox();
-            txtRegisterFirstName = new TextBox();
-            label6 = new Label();
-            label5 = new Label();
-            label2 = new Label();
-            label1 = new Label();
-            txtRegisterPassword = new TextBox();
-            txtRegisterUserName = new TextBox();
-            btnRegister = new Button();
-            btnRegisterNav = new Button();
-            btnLoginNav = new Button();
-            txtLoginPassword = new TextBox();
-            txtLoginUsername = new TextBox();
-            button3 = new Button();
-            label3 = new Label();
-            PanelLogin = new Panel();
-            label4 = new Label();
             RedisChat = new Label();
             btnClose = new Button();
             panel1 = new Panel();
-            PanelRegister.SuspendLayout();
-            PanelLogin.SuspendLayout();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // PanelRegister
-            // 
-            PanelRegister.BackColor = Color.Transparent;
-            PanelRegister.Controls.Add(txtRegisterLastName);
-            PanelRegister.Controls.Add(txtRegisterFirstName);
-            PanelRegister.Controls.Add(label6);
-            PanelRegister.Controls.Add(label5);
-            PanelRegister.Controls.Add(label2);
-            PanelRegister.Controls.Add(label1);
-            PanelRegister.Controls.Add(txtRegisterPassword);
-            PanelRegister.Controls.Add(txtRegisterUserName);
-            PanelRegister.Controls.Add(btnRegister);
-            PanelRegister.Location = new Point(26, 102);
-            PanelRegister.Margin = new Padding(3, 2, 3, 2);
-            PanelRegister.Name = "PanelRegister";
-            PanelRegister.Size = new Size(674, 337);
-            PanelRegister.TabIndex = 18;
-            // 
-            // txtRegisterLastName
-            // 
-            txtRegisterLastName.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            txtRegisterLastName.Location = new Point(248, 58);
-            txtRegisterLastName.Margin = new Padding(3, 2, 3, 2);
-            txtRegisterLastName.Name = "txtRegisterLastName";
-            txtRegisterLastName.Size = new Size(190, 29);
-            txtRegisterLastName.TabIndex = 16;
-            // 
-            // txtRegisterFirstName
-            // 
-            txtRegisterFirstName.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            txtRegisterFirstName.Location = new Point(247, 28);
-            txtRegisterFirstName.Margin = new Padding(3, 2, 3, 2);
-            txtRegisterFirstName.Name = "txtRegisterFirstName";
-            txtRegisterFirstName.Size = new Size(190, 29);
-            txtRegisterFirstName.TabIndex = 15;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(136, 62);
-            label6.Name = "label6";
-            label6.Size = new Size(94, 22);
-            label6.TabIndex = 14;
-            label6.Text = "Last Name";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(133, 30);
-            label5.Name = "label5";
-            label5.Size = new Size(98, 22);
-            label5.TabIndex = 13;
-            label5.Text = "First Name";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(136, 134);
-            label2.Name = "label2";
-            label2.Size = new Size(90, 22);
-            label2.TabIndex = 12;
-            label2.Text = "Password";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(133, 95);
-            label1.Name = "label1";
-            label1.Size = new Size(92, 22);
-            label1.TabIndex = 11;
-            label1.Text = "Username";
-            // 
-            // txtRegisterPassword
-            // 
-            txtRegisterPassword.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            txtRegisterPassword.Location = new Point(248, 132);
-            txtRegisterPassword.Margin = new Padding(3, 2, 3, 2);
-            txtRegisterPassword.Name = "txtRegisterPassword";
-            txtRegisterPassword.PasswordChar = '*';
-            txtRegisterPassword.Size = new Size(190, 29);
-            txtRegisterPassword.TabIndex = 10;
-            // 
-            // txtRegisterUserName
-            // 
-            txtRegisterUserName.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            txtRegisterUserName.Location = new Point(248, 93);
-            txtRegisterUserName.Margin = new Padding(3, 2, 3, 2);
-            txtRegisterUserName.Name = "txtRegisterUserName";
-            txtRegisterUserName.Size = new Size(190, 29);
-            txtRegisterUserName.TabIndex = 9;
-            // 
-            // btnRegister
-            // 
-            btnRegister.BackColor = Color.FromArgb(9, 89, 205);
-            btnRegister.Font = new Font("Cambria", 14F, FontStyle.Bold, GraphicsUnit.Point);
-            btnRegister.Location = new Point(179, 207);
-            btnRegister.Margin = new Padding(3, 2, 3, 2);
-            btnRegister.Name = "btnRegister";
-            btnRegister.Size = new Size(396, 44);
-            btnRegister.TabIndex = 8;
-            btnRegister.Text = "Register";
-            btnRegister.UseVisualStyleBackColor = false;
-            // 
-            // btnRegisterNav
-            // 
-            btnRegisterNav.BackColor = Color.Silver;
-            btnRegisterNav.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
-            btnRegisterNav.Location = new Point(358, 58);
-            btnRegisterNav.Margin = new Padding(3, 2, 3, 2);
-            btnRegisterNav.Name = "btnRegisterNav";
-            btnRegisterNav.Size = new Size(353, 40);
-            btnRegisterNav.TabIndex = 17;
-            btnRegisterNav.Text = "Register";
-            btnRegisterNav.UseVisualStyleBackColor = false;
-            // 
-            // btnLoginNav
-            // 
-            btnLoginNav.BackColor = Color.Silver;
-            btnLoginNav.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
-            btnLoginNav.Location = new Point(0, 58);
-            btnLoginNav.Margin = new Padding(3, 2, 3, 2);
-            btnLoginNav.Name = "btnLoginNav";
-            btnLoginNav.Size = new Size(353, 40);
-            btnLoginNav.TabIndex = 16;
-            btnLoginNav.Text = "Login";
-            btnLoginNav.UseVisualStyleBackColor = false;
-            // 
-            // txtLoginPassword
-            // 
-            txtLoginPassword.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            txtLoginPassword.Location = new Point(249, 134);
-            txtLoginPassword.Margin = new Padding(3, 2, 3, 2);
-            txtLoginPassword.Name = "txtLoginPassword";
-            txtLoginPassword.PasswordChar = '*';
-            txtLoginPassword.Size = new Size(190, 29);
-            txtLoginPassword.TabIndex = 10;
-            // 
-            // txtLoginUsername
-            // 
-            txtLoginUsername.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            txtLoginUsername.Location = new Point(249, 94);
-            txtLoginUsername.Margin = new Padding(3, 2, 3, 2);
-            txtLoginUsername.Name = "txtLoginUsername";
-            txtLoginUsername.Size = new Size(190, 29);
-            txtLoginUsername.TabIndex = 9;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.FromArgb(9, 89, 205);
-            button3.Font = new Font("Cambria", 14F, FontStyle.Bold, GraphicsUnit.Point);
-            button3.Location = new Point(181, 208);
-            button3.Margin = new Padding(3, 2, 3, 2);
-            button3.Name = "button3";
-            button3.Size = new Size(396, 44);
-            button3.TabIndex = 8;
-            button3.Text = "Login";
-            button3.UseVisualStyleBackColor = false;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(138, 136);
-            label3.Name = "label3";
-            label3.Size = new Size(90, 22);
-            label3.TabIndex = 12;
-            label3.Text = "Password";
-            // 
-            // PanelLogin
-            // 
-            PanelLogin.Controls.Add(label3);
-            PanelLogin.Controls.Add(label4);
-            PanelLogin.Controls.Add(txtLoginPassword);
-            PanelLogin.Controls.Add(txtLoginUsername);
-            PanelLogin.Controls.Add(button3);
-            PanelLogin.Location = new Point(26, 119);
-            PanelLogin.Margin = new Padding(3, 2, 3, 2);
-            PanelLogin.Name = "PanelLogin";
-            PanelLogin.Size = new Size(664, 309);
-            PanelLogin.TabIndex = 15;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Cambria", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(135, 97);
-            label4.Name = "label4";
-            label4.Size = new Size(92, 22);
-            label4.TabIndex = 11;
-            label4.Text = "Username";
             // 
             // RedisChat
             // 
@@ -283,50 +75,82 @@
             panel1.Size = new Size(724, 41);
             panel1.TabIndex = 14;
             // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(flowLayoutPanel1);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 41);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(193, 409);
+            panel2.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.BackgroundImage = Properties.Resources.img1;
+            pictureBox1.Image = Properties.Resources.img11;
+            pictureBox1.Location = new Point(62, 39);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(64, 64);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(0, 124);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(193, 282);
+            flowLayoutPanel1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(72, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 16;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(42, 2);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 25);
+            label2.TabIndex = 19;
+            label2.Text = "My Profile";
+            // 
             // HomeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(724, 450);
-            Controls.Add(PanelRegister);
-            Controls.Add(btnRegisterNav);
-            Controls.Add(btnLoginNav);
-            Controls.Add(PanelLogin);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "HomeForm";
             Text = "HomeForm";
-            PanelRegister.ResumeLayout(false);
-            PanelRegister.PerformLayout();
-            PanelLogin.ResumeLayout(false);
-            PanelLogin.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel PanelRegister;
-        private TextBox txtRegisterLastName;
-        private TextBox txtRegisterFirstName;
-        private Label label6;
-        private Label label5;
-        private Label label2;
-        private Label label1;
-        private TextBox txtRegisterPassword;
-        private TextBox txtRegisterUserName;
-        private Button btnRegister;
-        private Button btnRegisterNav;
-        private Button btnLoginNav;
-        private TextBox txtLoginPassword;
-        private TextBox txtLoginUsername;
-        private Button button3;
-        private Label label3;
-        private Panel PanelLogin;
-        private Label label4;
         private Label RedisChat;
         private Button btnClose;
         private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox pictureBox1;
+        private Label label2;
     }
 }
